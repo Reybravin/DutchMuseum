1825e25ebb1033863694d8cd1660f9eb04ddb8e4
Sergiy Sachuk <sachuksv@gmail.com>
2024-01-26 16:50:21 +0100

    added search bar

--- DutchMuseum/Scenes/Museum/ArtList/ArtListViewController.swift
+++ DutchMuseum/Scenes/Museum/ArtList/ArtListViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 final class ArtListViewController: UIViewController, IAlert {
     
     // MARK: - Private properties
-    
+    private let searchController = UISearchController(searchResultsController: nil)
     private var collectionView: ArtListCollectionView?
-    
+
     //MARK: - Public propertis
     
     weak var coordinator: RootCoordinator?
@@ -25,6 +25,20 @@ final class ArtListViewController: UIViewController, IAlert {
         
         addCollectionView()
         bind(to: viewModel)
+        
+        
+        searchController.searchResultsUpdater = self
+        searchController.delegate = self
+        searchController.searchBar.delegate = self
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search..."
+        searchController.searchBar.sizeToFit()
+        searchController.searchBar.becomeFirstResponder()
+        navigationItem.titleView = searchController.searchBar
+
+        definesPresentationContext = true
+        searchController.searchBar.placeholder = "Search for Items"
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -66,7 +80,60 @@ final class ArtListViewController: UIViewController, IAlert {
         }
     }
 }
- 
+
+extension ArtListViewController: UISearchControllerDelegate, UISearchBarDelegate, UISearchResultsUpdating {
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        //searchActive = false
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func updateSearchResults(for searchController: UISearchController)
+    {
+        let searchString = searchController.searchBar.text
+        
+//        filtered = items.filter({ (item) -> Bool in
+//            let countryText: NSString = item as NSString
+//            
+//            return (countryText.range(of: searchString!, options: NSString.CompareOptions.caseInsensitive).location) != NSNotFound
+//        })
+//        
+//        collectionView.reloadData()
+    }
+    
+//    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+//        searchActive = true
+//        collectionView.reloadData()
+//    }
+    
+//    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+//        searchActive = false
+//        collectionView.reloadData()
+//    }
+    
+//    func searchBarBookmarkButtonClicked(_ searchBar: UISearchBar) {
+//        if !searchActive {
+//            searchActive = true
+//            collectionView.reloadData()
+//        }
+//        
+//        searchController.searchBar.resignFirstResponder()
+//    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        NSObject.cancelPreviousPerformRequests(withTarget: self, selector: #selector(onSearchTextChange), object: searchBar)
+        perform(#selector(onSearchTextChange), with: searchBar, afterDelay: 0.5)
+    }
+    
+    @objc private func onSearchTextChange(_ searchBar: UISearchBar) {
+        guard let query = searchBar.text, query.trimmingCharacters(in: .whitespaces) != "" else {
+            print("nothing to search")
+            return
+        }
+        viewModel?.didEnterSearchQuery(query)
+        print(query)
+    }
+}
+
 // MARK: - ArtListCollectionViewDelegate
 
 extension ArtListViewController: ArtListCollectionViewDelegate {
